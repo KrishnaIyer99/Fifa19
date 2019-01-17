f83cefd23a9cbf894c86fb6f0a5bc2e216f68b4e
KrishnaIyer99 <32946254+KrishnaIyer99@users.noreply.github.com>
2019-01-16 23:58:34 -0500

    create dataframe

--- fastestCountry.R
+++ fastestCountry.R
@@ -2,4 +2,14 @@
 library(readxl)
 
 #import dataset
-rawData <- read.csv("C:/Users/krish/Downloads/fifa19/data.csv")
+rawData <- read.csv("C:/Users/krish/Desktop/fifa19/data.csv")
+
+#filter out unecessary columns
+name <- (rawData$'Name')
+nationality <- (rawData$'Nationality')
+overallStat <- (rawData$Overall)
+club <- (rawData$'Club')
+position <- (rawData$'Position')
+sprintSpeed <- (rawData$SprintSpeed)
+
+df1 <- data.frame(name, nationality, overallStat, club, position, sprintSpeed)
