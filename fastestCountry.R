9956cd53b17aae4eed8093a3119cd197b26dbdae
KrishnaIyer99 <32946254+KrishnaIyer99@users.noreply.github.com>
2019-01-17 23:09:08 -0500

    removed unnecessary data and split data by country

--- fastestCountry.R
+++ fastestCountry.R
@@ -4,15 +4,15 @@ rawData <- read.csv("C:/Users/krish/Desktop/fifa19/data.csv")
 #filter out unecessary columns
 name <- (rawData$'Name')
 nationality <- (rawData$'Nationality')
-overallStat <- (rawData$Overall)
-club <- (rawData$'Club')
 sprintSpeed <- (rawData$SprintSpeed)
 
 #create dataframe
-df1 <- data.frame(name, nationality, overallStat, club, sprintSpeed)
+df1 <- data.frame(name, nationality, sprintSpeed)
 
 #Remove NA values for sprint speed
 df1 <- df1[!is.na(df1$sprintSpeed),]
-df1 <- df1[order(df1$sprintSpeed, decreasing = TRUE),]
+df1 <- df1[order(df1$nationality, decreasing = FALSE),]
 
-countries <- data.frame(unique(nationality)) #removes duplicates
+countries <- split(df1, df1$nationality) #group data by country
+
+
