71c64a474647871b89016885a0205ce3c333641f
KrishnaIyer99 <32946254+KrishnaIyer99@users.noreply.github.com>
2019-01-20 13:35:16 -0500

    include sample size of each country

--- fastestCountry.R
+++ fastestCountry.R
@@ -6,6 +6,7 @@ name <- (rawData$'Name')
 nationality <- (rawData$'Nationality')
 sprintSpeed <- (rawData$SprintSpeed)
 
+
 #create dataframe
 df1 <- data.frame(name, nationality, sprintSpeed)
 
@@ -16,20 +17,22 @@ df1 <- df1[order(df1$nationality, decreasing = FALSE),]
 countries <- split(df1, df1$nationality) #group data by country
 
 country <- unique(df1$nationality)
+numPlayers <- vector()
 average <- vector()
 stdDeviation <- vector()
 maxSpd <- vector()
 minSpd <- vector()
 
-#collect average, standard deviation, max and min speeds of each country
+#collect sample size, average, standard deviation, max and min speeds of each country
 for(i in c(1:length(countries))){
+  numPlayers[i] <- length(countries[[i]]$sprintSpeed) 
   average[i] <- mean(countries[[i]]$sprintSpeed)
   stdDeviation[i] <- sd(countries[[i]]$sprintSpeed)
   maxSpd[i] <- max(countries[[i]]$sprintSpeed)
   minSpd[i] <- min(countries[[i]]$sprintSpeed) 
 }
 
-countryStats <- data.frame(country, average, stdDeviation, maxSpd, minSpd)
+countryStats <- data.frame(country, numPlayers, average, stdDeviation, maxSpd, minSpd) 
 
 
 
